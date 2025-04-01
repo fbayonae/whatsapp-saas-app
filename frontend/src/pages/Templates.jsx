06af4ef6129a3a3bf88b5bfc461d83889d927778
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-01 20:28:57 +0200

    auto commit

--- frontend/src/pages/Templates.jsx
+++ frontend/src/pages/Templates.jsx
@@ -1,4 +1,13 @@
-export default function Templates() {
+/*export default function Templates() {
     return <h1 className="text-2xl font-semibold">Gestión de Plantillas</h1>;
+}*/
+
+export default function Templates() {
+    return (
+      <div className="bg-indigo-600 text-white text-2xl font-bold p-8">
+        ¡Hola desde Tailwind!
+      </div>
+    );
   }
+  
   

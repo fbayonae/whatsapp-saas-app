95760c51e0d90d9663712c79bed7516d08cf04fe
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-03 17:19:21 +0200

    auto commit

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient();
 
@@ -32,3 +32,4 @@ export const registerUser = async (req, res) => {
     return res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const whatsappRoutes = require('./routes/whatsapp');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use('/api', whatsappRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {

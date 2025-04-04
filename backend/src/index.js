602ffcfab1b59da3c33af3e348af5fde0126f858
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-04 18:16:26 +0200

    auto commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // necesario para leer POST de Meta
 app.use(express.json({ limit: '5mb' })); 
-app.use(express.json());
+//app.use(express.json());
 
 app.use('/api/templates', templatesRoutes);
 app.use('/auth', authRoutes);

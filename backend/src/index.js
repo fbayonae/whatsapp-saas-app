c1a9f41e0372baeff8e01fc6358d1cf43949ca8e
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-08 11:06:55 +0200

    auto commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,21 +5,19 @@ const authRoutes = require('./routes/authRoutes');
 const templatesRoutes = require('./routes/templatesRoutes');
 const webhookRoutes = require('./routes/webhookRoutes');
 const contactRoutes = require("./routes/contactRoutes");
-
+const chatRoutes = require("./routes/chatRoutes");
 
 dotenv.config();
 const app = express();
 
-//app.use('/api', whatsappRoutes);
-
 // necesario para leer POST de Meta
 app.use(express.json({ limit: '5mb' })); 
-//app.use(express.json());
 
 app.use('/api/templates', templatesRoutes);
 app.use('/auth', authRoutes);
 app.use('/webhook', webhookRoutes);
 app.use("/api/contacts", contactRoutes);
+app.use("/api/chats", chatRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {

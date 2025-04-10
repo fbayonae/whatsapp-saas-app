88dc853fcf37603c7114d956cdaee212d26077d4
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-10 10:23:53 +0200

    auto commit

--- backend/src/utils/multerUtils.js
+++ backend/src/utils/multerUtils.js
@@ -1,9 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+// Ruta absoluta segura dentro del contenedor
+const uploadPath = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/src/uploads/");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);

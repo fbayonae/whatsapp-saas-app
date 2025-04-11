b3c92d73e9d04e3da15a8af00d0f6eab2107211f
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-11 12:40:56 +0200

    auto commit

--- backend/src/utils/authMiddleware.js
+++ backend/src/utils/authMiddleware.js
@@ -2,17 +2,18 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
     const authHeader = req.headers["authorization"];
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    const token = authHeader?.split(" ")[1]; // Bearer <token>
+  
+    if (!token) {
       return res.status(401).json({ error: "Token requerido" });
     }
   
-    const token = authHeader.split(" ")[1];
-  
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-      req.user = decoded; // se puede usar después en las rutas
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded; // puedes usar req.user.userId, etc.
       next();
     } catch (err) {
+      console.error("❌ Token inválido:", err.message);
       return res.status(403).json({ error: "Token inválido o expirado" });
     }
 };

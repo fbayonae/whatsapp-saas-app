e8d66a227c95a0a71bc476f84b06edca1efba519
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-11 12:32:28 +0200

    auto commit

--- backend/src/utils/authMiddleware.js
+++ backend/src/utils/authMiddleware.js
@@ -4,6 +4,7 @@ const auth = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Token requerido" });
   console.log("Token: ", token);
+  console.log("Secret: ", process.env.JWT_SECRET_KEY);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     console.log("Decoded: ", decoded);

03c1172cbf09ce6afa3f0571b31ced3a066e8220
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-11 12:39:30 +0200

    auto commit

--- backend/src/routes/templatesRoutes.js
+++ backend/src/routes/templatesRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const templateController = require('../controllers/templateController');
-const auth = require("../utils/authMiddleware");
+const auth = require("../utils/authMiddleware").auth;
 
 const router = express.Router();
 
-router.post('/sync', auth.auth, templateController.syncTemplates);
-router.get('/', auth.auth, templateController.getTemplates);
+router.post('/sync', auth, templateController.syncTemplates);
+router.get('/', auth, templateController.getTemplates);
 
 module.exports = router;

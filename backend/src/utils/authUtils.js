4e3e9ec8163fc3d0dcb9619179ea6a73c3d2e4bd
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-11 10:24:39 +0200

    auto commit

--- backend/src/utils/authUtils.js
+++ backend/src/utils/authUtils.js
@@ -9,7 +9,7 @@ const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded; // se puede usar después en las rutas
     next();
   } catch (err) {

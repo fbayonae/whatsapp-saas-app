7696c830bee66302d279f8134f3ee7a20a153005
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-13 11:30:08 +0200

    Actualización automática

--- backend/src/routes/messagesRoutes.js
+++ backend/src/routes/messagesRoutes.js
@@ -7,5 +7,6 @@ const auth = require("../utils/authMiddleware").auth;
 
 router.post("/send", auth,  messageController.sendMessage);
 router.post("/send-media", auth, upload.single("file"), messageController.sendMessageMedia);
+router.post("/send-cta", auth, messageController.sendMessageCTA);
 
 module.exports = router;

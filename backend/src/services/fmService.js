afce154b0df323d8904411e85f1a0196d0756a67
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-24 12:24:55 +0200

    Actualización automática

--- backend/src/services/fmService.js
+++ backend/src/services/fmService.js
@@ -28,7 +28,7 @@ const login = async () => {
     },
     headers: { "Content-Type": "application/json" }
   });
-  console.log("Respuesta de FileMaker:", response);
+  console.log("Respuesta de FileMaker:", response.data);
   token = response.data.response.token;
   return token;
 };

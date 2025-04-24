b13191c96301c81093cb39d8896c042f4bdbee95
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-24 14:28:19 +0200

    Actualización automática

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -64,7 +64,7 @@ const login = async (req, res) => {
       data: {
         userId: user.id,
         ip,
-        userAgent,
+        userAgent || null,
         refreshToken,
         token: refreshToken,
         createdAt: new Date(),

b4cc2ebca964d0f64312491af17c30768f638812
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-30 19:58:08 +0200

    Actualización automática

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "../utils/axiosInstance";
 import { Paperclip, X, LayoutTemplate, MessageSquareText } from "lucide-react";
 import MessageModalTemplate from "../components/MessageModalTemplate";

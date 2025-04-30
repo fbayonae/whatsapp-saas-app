3084ef3ddd2cf7b974f114b21cab9d185be00280
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-04-30 17:48:54 +0200

    Actualización automática

--- backend/src/utils/validators.js
+++ backend/src/utils/validators.js
@@ -1,7 +1,6 @@
 const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
-  console.log(req.body);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({

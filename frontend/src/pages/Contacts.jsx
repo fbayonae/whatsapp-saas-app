3d24120d5ab5620dce4b22ad04415958fc428e93
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-05-03 16:55:30 +0200

    Actualización automática

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "../utils/axiosInstance";
-import CreateContactModal from "./components/CreateContactModal";
+import CreateContactModal from "../components/CreateContactModal";
 
 export default function Contacts() {
   const [contacts, setContacts] = useState([]);

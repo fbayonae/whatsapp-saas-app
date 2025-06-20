c83013e4a094c94c69d264d30e24db0a7f432072
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-06-20 20:35:21 +0200

    Actualización automática

---
+++ backend/src/domains/whatsapp/chats/routes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { param, body } = require("express-validator");
+
+const router = express.Router();
+
+// Controllers
+const chatController = require("../controllers/chatController");
+
+// Middleware
+const auth = require("../../../core/middleware/auth").auth;
+const validate = require('../../core/middleware/validators');
+
+router.get("/",
+    auth,
+    chatController.getConversations
+);
+
+router.post("/",
+    auth,
+    [
+        body("contactId")
+            .isInt({ min: 1 })
+            .withMessage("El ID del contacto debe ser un número entero positivo")
+    ],
+    chatController.createConversation
+);
+
+router.get("/:id/check-window",
+    auth,
+    [
+        param("id")
+            .isInt({ min: 1 })
+            .withMessage("El ID de la conversación debe ser un número entero positivo")
+    ],
+    validate,
+    chatController.checkWindow24h
+);
+
+router.get("/:id/messages",
+    auth,
+    [
+        param("id")
+            .isInt({ min: 1 })
+            .withMessage("El ID de la conversación debe ser un número entero positivo")
+    ],
+    validate,
+    chatController.getMessagesByConversation
+);
+
+module.exports = router;
---
+++ backend/src/domains/whatsapp/contacts/routes.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { body, param } = require("express-validator");
+
+const router = express.Router();
+
+// Controllers
+const contactController = require("../controllers/contactController");
+
+// Middlewares
+const auth = require("../../../core/middleware/auth").auth;
+const validate = require('../../core/middleware/validators');
+
+router.get("/",
+    auth,
+    contactController.getAllContacts
+);
+
+router.post("/",
+    auth,
+    [
+        body("phoneNumber")
+            .notEmpty().withMessage("El número de teléfono es obligatorio")
+            .isString().withMessage("El número de teléfono debe ser un string"),
+        body("name")
+            .notEmpty().withMessage("El nombre es obligatorio")
+            .isString().withMessage("El nombre debe ser un string"),
+    ],
+    contactController.createContact
+);
+
+router.put("/:id",
+    auth,
+    [
+        param("id")
+            .isInt({ min: 1 })
+            .withMessage("El ID del contacto debe ser un número entero positivo"),
+            body("name").notEmpty().withMessage("El nombre es obligatorio"),
+            body("phoneNumber").notEmpty().withMessage("El número es obligatorio"),
+    ],
+    contactController.updateContact
+);
+
+router.delete("/:id",
+    auth,
+    [
+        param("id")
+            .isInt({ min: 1 })
+            .withMessage("El ID del contacto debe ser un número entero positivo")
+    ],
+    contactController.deleteContact
+);
+
+router.get("/:id/conversations",
+    auth,
+    [
+        param("id")
+            .isInt({ min: 1 })
+            .withMessage("El ID del contacto debe ser un número entero positivo")
+    ],
+    contactController.getConversationsByContact
+);
+
+module.exports = router;

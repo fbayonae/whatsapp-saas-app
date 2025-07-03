35a97a01df25eb45effc221dacec8996cd243a63
Francisco Jesus Bayona <fbayona@MacBook-Pro-de-Francisco.local>
2025-07-03 13:24:42 +0200

    Actualización automática

--- backend/src/core/logs/routes.js
+++ backend/src/core/logs/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const logsController = require("../controllers/logsController");
+const logsController = require("./controllers/logsController");
 
 // Middleware
 const auth = require("./middleware/auth").auth;
